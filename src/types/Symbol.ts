e6d59bf1f770a4c52515eade477eae855ac20c7e
Mary Kilduff <marykilduff@Marys-MacBook-Pro.local>
2023-06-13 18:23:53 +0100

    refactoring

--- src/types/Symbol.ts
+++ src/types/Symbol.ts
@@ -2,20 +2,4 @@ export interface SymbolType {
   symbol: string;
   status: string;
   baseAsset: string;
-  baseAssetPrecision: number;
-  quoteAsset: string;
-  quotePrecision: number;
-  quoteAssetPrecision: number;
-  baseCommissionPrecision: number;
-  quoteCommissionPrecision: number;
-  orderTypes: string[];
-  ocoAllowed: boolean;
-  quoteOrderQtyMarketAllowed: boolean;
-  allowTrailingStop: boolean;
-  cancelReplaceAllowed: boolean;
-  isSpotTradingAllowed: boolean;
-  isMarginTradingAllowed: boolean;
-  permissions: string[];
-  defaultSelfTradePreventionMode: string;
-  allowedSelfTradePreventionModes: string[];
 }
--- src/types/Ticker.ts
+++ src/types/Ticker.ts
@@ -3,13 +3,8 @@ export interface TickerType {
   priceChange: string;
   priceChangePercent: string;
   weightedAvgPrice: string;
-  prevClosePrice: string;
   lastPrice: string;
-  lastQty: string;
   bidPrice: string;
-  bidQty: string;
-  askPrice: string;
-  askQty: string;
   openPrice: string;
   highPrice: string;
   lowPrice: string;
